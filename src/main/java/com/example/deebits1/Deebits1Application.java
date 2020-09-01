c1d83b8f2137f8de3febc2a047171fdd58eefc7b
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-09-02 02:27:35 +0530

    initial3

--- src/main/java/com/example/deebits1/Deebits1Application.java
+++ src/main/java/com/example/deebits1/Deebits1Application.java
@@ -1,13 +1,13 @@
-package com.example.deebtis1;
+package com.example.deebits1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Deebtis1Application {
+public class Deebits1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Deebtis1Application.class, args);
+		SpringApplication.run(Deebits1Application.class, args);
 	}
 
 }
