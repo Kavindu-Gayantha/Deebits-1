c259252e9f98790343a29ee25629e0a229ae8de8
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-09-01 21:32:22 +0530

    initial

---
+++ src/test/java/com/example/deebtis1/Deebtis1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.deebtis1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Deebtis1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
