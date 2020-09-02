3e5e059623a3134686eefe711051d38ad7e9b5d7
kavindu-gayantha <96kavindugayantha@gmail.com>
2020-09-02 19:50:35 +0530

    finished making end points

--- src/main/java/com/example/deebits1/controller/BookController.java
+++ src/main/java/com/example/deebits1/controller/BookController.java
@@ -1,24 +1,61 @@
 package com.example.deebits1.controller;
 
 import com.example.deebits1.model.Books;
-import com.example.deebits1.repository.BookRepository;
+import com.example.deebits1.services.BookServices;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
+// end points are located here
 
 @RestController
 @RequestMapping("/api")
 public class BookController {
 
-    @Autowired
-    private BookRepository bookRepository;
+//    @Autowired
+//    private BookRepository bookRepository;
+//
+//    @GetMapping("allBooks") //end point - get
+//    public List<Books> getBooks(){
+//        return this.bookRepository.findAll();
+//    }
 
-    @GetMapping("books") //end point - get
-    public List<Books> getBooks(){
-        return this.bookRepository.findAll();
+    @Autowired
+    private BookServices bookServices;
+
+    //get -end point to get all the books
+    @GetMapping("/books")
+    public ResponseEntity<List<Books>> getAllBooks(){
+        return ResponseEntity.ok().body(bookServices.getAllBooks());
+    }
+    @GetMapping("/books/{id}")
+    public ResponseEntity<Object> getBookById(@PathVariable long id){
+        return ResponseEntity.ok().body(bookServices.getBookById(id));
+    }
+
+    //post -end point
+    @PostMapping("/addBook")
+    public ResponseEntity<Books> addBook(@RequestBody Books books){
+//        this.bookServices.createBook(books);
+          return ResponseEntity.ok().body(this.bookServices.createBook(books));
+
+    }
+    //update -end point
+    @PutMapping("/updateBook/{id}")
+    public ResponseEntity<Books> updateBook(@PathVariable long id, @RequestBody Books books){
+        books.setId(id);
+        return ResponseEntity.ok().body(this.bookServices.updateBook(books));
+
+    }
+    //delete -end point
+    @DeleteMapping("/deleteBook/{id}")
+    public HttpStatus deleteBook(@PathVariable long id){
+        this.bookServices.deleteBook(id);
+
+        return HttpStatus.OK;
     }
 
 
